a52df0d6f50c3cb95f5ea662e0e2753d5f44b44a
Sculas <contact@sculas.xyz>
2022-07-10 01:12:50 +0200

    fix: unique constraint not working because of invalid struct tag

--- service/database/models/cameras.go
+++ service/database/models/cameras.go
@@ -1,8 +1,8 @@
 package models
 
 type Camera struct {
-	Uuid      string `gorm:"primaryKey,unique_index:idx_uuid2name" json:"uuid"`
-	Name      string `gorm:"not null,unique_index:idx_uuid2name" json:"name"`
+	Uuid      string `gorm:"primaryKey,uniqueIndex:idx_uuid2name" json:"uuid"`
+	Name      string `gorm:"not null,uniqueIndex:idx_uuid2name" json:"name"`
 	OwnerUuid string `gorm:"not null" json:"ownerUuid"`
 
 	IPAddress string     `gorm:"not null" json:"addr"`
